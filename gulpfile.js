6fdf2140e6c1c078088b45af37a1bb2e8e0f7575
Y.Kogawa <ykogawa4@gmail.com>
2015-07-14 17:15:00 +0900

    スライドの切り替えを操作するボタンと機能を追加

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,12 @@ var setting = {
     // 使わない方はコメントアウトする
     // proxy: 'environment.yk',
     server:{
-        baseDir: 'www',
+        baseDir: 'build',
     },
+  },
+  imagemin: {
+    disabled: true,  // falseでimageminを実行
+    level: 7  // 圧縮率
   },
   // css、jsのミニファイの有効化/無効化
   minify: {
@@ -21,33 +25,74 @@ var setting = {
     js: false
   },
   cssbeautify: {
-    disabled: false,
+    disabled: true,
     options: {
       indent: '	'
     }
   },
   csscomb: {
-    disabled: false,
+    disabled: true,
   },
   path: {
     base: {
       src: 'src',
-      dest: 'httpdocs'
+      dest: 'build'
     },
     sass: {
-      src: ['www/**/*.scss', '!www/asset/**/*', '!www/unorganized/**/*'],
-      dest: 'www/',
-    },
-    css_beautify: {
-      src: ['www/**/*.css', '!www/asset/**/*.css', '!www/unorganized/**/*.css'],
-      dest: 'www/',
+      src: 'src/assets/sass/**/*.scss',
+      dest: 'build/assets/css/',
     },
     js: {
-      src: ['www/assets/js/**/*.js', '!www/asset/**/*', '!www/unorganized/**/*'],
-      dest: 'www/assets/js/',
+      src: 'src/assets/js/**/*.js',
+      dest: 'build/assets/js/',
+    },
+    image: {
+      src: 'src/assets/img/**/*',
+      dest: 'build/assets/img/',
+    },
+    lib: {
+      src: 'src/assets/lib/**/*',
+      dest: 'build/assets/lib/',
+    },
+    include: {
+      src: ['src/assets/include/**/*', '!src/assets/include/**/_*.ejs'],
+      dest: 'build/assets/include/',
+    },
+    etc: {
+      src: 'src/assets/etc/**/*',
+      dest: 'build/assets/etc/',
+    },
+    ejs: {
+      watch: 'src/**/*.ejs',
+      src: ['src/**/*.ejs', '!src/**/_*.ejs']
+    },
+    html: {
+      src: ['src/**/*', '!src/assets/**/*', '!src/**/*.ejs']
     },
   }
 };
+
+// 画像の圧縮
+gulp.task('imagemin', function(){
+  if(!setting.imagemin.disabled){
+    var imageminOptions = {
+      optimizationLevel: setting.imagemin.lebel
+    };
+
+    return gulp.src(setting.path.image.src)
+      .pipe($.changed(setting.path.image.dest))
+      .pipe($.imagemin(imageminOptions))
+      .pipe(gulp.dest(setting.path.image.dest))
+      .pipe(browserSync.reload({stream: true}));
+  }else{
+    return gulp.src(
+        setting.path.image.src
+      )
+      .pipe($.changed(setting.path.image.dest))
+      .pipe(gulp.dest(setting.path.image.dest))
+      .pipe(browserSync.reload({stream: true}));
+  }
+});
 
 // SASS
 gulp.task('scss',function(){
@@ -59,6 +104,67 @@ gulp.task('scss',function(){
     .pipe($.autoprefixer(setting.autoprefixer.browser))
     .pipe(gulp.dest(setting.path.sass.dest))
     .pipe(browserSync.reload({stream: true}));
+});
+
+// EJS
+gulp.task('ejs', function(){
+  return gulp.src(
+    setting.path.ejs.src
+  )
+  .pipe($.ejs())
+  .pipe(gulp.dest(setting.path.base.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// HTML
+gulp.task('html', function(){
+  return gulp.src(
+    setting.path.html.src,
+    {base: setting.path.base.src}
+  )
+  .pipe($.changed(setting.path.base.dest))
+  .pipe(gulp.dest(setting.path.base.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// JavaScript
+gulp.task('js', function(){
+  return gulp.src(
+    setting.path.js.src
+  )
+  .pipe($.changed(setting.path.js.dest))
+  .pipe(gulp.dest(setting.path.js.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// Lib
+gulp.task('lib', function(){
+  return gulp.src(
+    setting.path.lib.src
+  )
+  .pipe($.changed(setting.path.lib.dest))
+  .pipe(gulp.dest(setting.path.lib.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// Include
+gulp.task('include', function(){
+  return gulp.src(
+    setting.path.include.src
+  )
+  .pipe($.changed(setting.path.include.dest))
+  .pipe(gulp.dest(setting.path.include.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// Etc
+gulp.task('etc', function(){
+  return gulp.src(
+    setting.path.etc.src
+  )
+  .pipe($.changed(setting.path.etc.dest))
+  .pipe(gulp.dest(setting.path.etc.dest))
+  .pipe(browserSync.reload({stream: true}));
 });
 
 // JS Minify
@@ -82,9 +188,9 @@ gulp.task('cssminify', function(){
 // CSS Beautify
 gulp.task('cssbeautify', function(){
   if(!setting.cssbeautify.disabled && !setting.minify.css){
-    return gulp.src(setting.path.css_beautify.src)
+    return gulp.src(setting.path.sass.dest+'**/*.css')
       .pipe($.cssbeautify(setting.cssbeautify.options))
-      .pipe(gulp.dest(setting.path.css_beautify.dest));
+      .pipe(gulp.dest(setting.path.sass.dest));
   }
 });
 
@@ -103,8 +209,10 @@ gulp.task('clean', del.bind(null, setting.path.base.dest));
 // Build
 gulp.task('build', function(){
   return runSequence(
-    ['scss'],
-    ['cssbeautify']
+    ['clean'],
+    ['html', 'ejs', 'js', 'scss', 'lib', 'include', 'etc'],
+    ['csscomb'],
+    ['imagemin', 'cssminify', 'jsminify', 'cssbeautify']
   );
 });
 
@@ -113,7 +221,13 @@ gulp.task('watch', function(){
   browserSync.init(setting.browserSync);
 
   gulp.watch([setting.path.sass.src], ['scss']);
-  gulp.watch('www/**/*', browserSync.reload);
+  gulp.watch([setting.path.ejs.watch], ['ejs']);
+  gulp.watch([setting.path.js.src], ['js']);
+  gulp.watch([setting.path.lib.src], ['lib']);
+  gulp.watch([setting.path.include.src], ['include']);
+  gulp.watch([setting.path.etc.src], ['etc']);
+  gulp.watch([setting.path.html.src], ['html']);
+  gulp.watch([setting.path.image.src], ['imagemin']);
 });
 
 gulp.task('default',['watch']);
